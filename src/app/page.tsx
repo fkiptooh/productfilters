9b7ef451f3343420dbfb8b6cfd877fb593e2da12
Felix Kiptoo <fkiptooh@gmail.com>
2024-04-13 20:45:01 +0300

    feat(products) - zod validation, frontend color filter implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-"use client"
+"use client";
 import { Products } from "@/components/product/product";
 import { ProductSkeleton } from "@/components/product/product-skeleton";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import type { Product as TProduct } from "@/db";
 import { cn } from "@/lib/utils";
+import { ProductState } from "@/lib/validator/product-validator";
+import { AccordionContent } from "@radix-ui/react-accordion";
 import { useQuery } from "@tanstack/react-query";
 import { QueryResult } from "@upstash/vector";
 import axios from "axios";
@@ -11,71 +22,174 @@ import { ChevronDown, Filter } from "lucide-react";
 import { useState } from "react";
 
 const SORT_OPTIONS = [
-  {name: 'None', value: 'none'},
-  {name: 'Price: Low to High', value: 'price-asc'},
-  {name: 'Price: High to Low', value: 'price-desc'},
+  { name: "None", value: "none" },
+  { name: "Price: Low to High", value: "price-asc" },
+  { name: "Price: High to Low", value: "price-desc" },
 ] as const;
-export default function Home() {
-  const [filter, setFilter] = useState({
-    sort: 'none'
-  })
 
+const SUBCATEGORIES = [
+  { name: "T-Shirts", selected: true, href: "#" },
+  { name: "Hoodies", selected: false, href: "#" },
+  { name: "Sweatshirts", selected: false, href: "#" },
+  { name: "Accessories", selected: false, href: "#" },
+];
+
+const COLOR_FILTER = {
+  id: "color",
+  name: "Color",
+  options: [
+    { value: "white", label: "White" },
+    { value: "beige", label: "Beige" },
+    { value: "blue", label: "Blue" },
+    { value: "green", label: "Green" },
+    { value: "purple", label: "Purple" },
+  ] as const
+};
+
+const DEFAULT_CUSTOM_PRICE = [0, 100] as [number, number];
+
+export default function Home() {
+  const [filter, setFilter] = useState<ProductState>({
+    color: ["white", "blue", "green", "purple", "beige"],
+    price: { isCustom: false, range: DEFAULT_CUSTOM_PRICE },
+    size: ["L", "S", "M"],
+    sort: "none",
+  });
+
+  console.log(filter);
   const { data: products } = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
       const { data } = await axios.post<QueryResult<TProduct>[]>(
-        'http://localhost:3000/api/products',
+        "http://localhost:3000/api/products",
         {
-          sort: filter.sort
+          sort: filter.sort,
         }
       );
       return data;
+    },
+  });
+  const applyArrayFilter = ({
+    category,
+    value,
+  }: {
+    category: keyof Omit<typeof filter, "price" | "sort">;
+    value: string;
+  }) => {
+    const isFilterApplied = filter[category].includes(value as never);
+
+    if (isFilterApplied) {
+      setFilter((prev) => ({
+        ...prev,
+        [category]: prev[category].filter((v) => v !== value),
+      }));
+    } else {
+      setFilter((prev) => ({
+        ...prev,
+        [category]: [...prev[category], value],
+      }));
     }
-  })
+  };
 
   console.log(products);
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900">High Quality cotton selection.</h1>
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+          High Quality cotton selection.
+        </h1>
         <div className="flex items-center">
           <DropdownMenu>
             <DropdownMenuTrigger className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
               Sort
-              <ChevronDown className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"/>
+              <ChevronDown className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
               {SORT_OPTIONS.map((option) => (
-                <button 
-                  key={option.name} 
+                <button
+                  key={option.name}
                   className={cn("text-left w-full block px-4 py-2 text-sm", {
-                    'text-gray-900 bg-gray-100' : option.value === filter.sort,
-                    'text-gray-500' : option.value !== filter.sort
+                    "text-gray-900 bg-gray-100": option.value === filter.sort,
+                    "text-gray-500": option.value !== filter.sort,
                   })}
-                  onClick={() => setFilter((prev) => ({
-                  ...prev,
-                  sort: option.value
-                }))}>{option.name}</button>
+                  onClick={() =>
+                    setFilter((prev) => ({
+                      ...prev,
+                      sort: option.value,
+                    }))
+                  }
+                >
+                  {option.name}
+                </button>
               ))}
             </DropdownMenuContent>
           </DropdownMenu>
           <button className="-m-2 ml-4 p-2 text-gray-500 hover:text-gray-600 sm:ml-6 lg:hidden">
-            <Filter className="h-5 w-5"/>
+            <Filter className="h-5 w-5" />
           </button>
         </div>
       </div>
       <section className="pb-24 pt-6">
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
           {/* filters */}
-          <div></div>
+          <div className="hidden lg:block">
+            <ul className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
+              {SUBCATEGORIES.map((category) => (
+                <li key={category.name}>
+                  <button
+                    disabled={!category.selected}
+                    className="disabled:cursor-not-allowed disabled:opacity-60"
+                  >
+                    {category.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <Accordion type="multiple" className="animate-none">
+              {/* color-item */}
+              <AccordionItem value="color">
+                <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
+                  <span className="font-medium text-gray-900">Color</span>
+                </AccordionTrigger>
+                <AccordionContent className="pb-6 animate-none">
+                  <ul className="space-y-4">
+                    {COLOR_FILTER.options.map((option, optionIdx) => (
+                      <li key={option.value} className="flex items-center">
+                        <input
+                          type="checkbox"
+                          id={`color-${optionIdx}`}
+                          onChange={() => {
+                            applyArrayFilter({
+                              category: "color",
+                              value: option.value
+                            })
+                          }}
+                          checked={filter.color.includes(option.value)}
+                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        />
+                        <label
+                          htmlFor={`color-${optionIdx}`}
+                          className="ml-3 text-sm text-gray-500"
+                        >
+                          {option.label}
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
 
           {/* product grid */}
           <div className="lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-            {products ? products.map((product) => (
-              <Products product={product.metadata!} key={product.id}/>
-            )) :  new Array(12).fill(null).map((_, i) => (
-              <ProductSkeleton key={i}/>
-            ))}
+            {products
+              ? products.map((product) => (
+                  <Products product={product.metadata!} key={product.id} />
+                ))
+              : new Array(12)
+                  .fill(null)
+                  .map((_, i) => <ProductSkeleton key={i} />)}
           </div>
         </div>
       </section>
