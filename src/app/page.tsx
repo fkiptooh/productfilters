f4d95755046ac87b9c26312ac0cfab2237252489
Felix Kiptoo <fkiptooh@gmail.com>
2024-04-14 00:30:04 +0300

    feat(products) - implemented filter based on size frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,16 @@ const COLOR_FILTER = {
   ] as const
 };
 
+const SIZE_FILTER = {
+  id: "size",
+  name: "Size",
+  options: [
+    { value: "S", label: "S" },
+    { value: "M", label: "M" },
+    { value: "L", label: "L" },
+  ],
+} as const;
+
 const DEFAULT_CUSTOM_PRICE = [0, 100] as [number, number];
 
 export default function Home() {
@@ -165,6 +175,38 @@ export default function Home() {
                             })
                           }}
                           checked={filter.color.includes(option.value)}
+                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        />
+                        <label
+                          htmlFor={`color-${optionIdx}`}
+                          className="ml-3 text-sm text-gray-500"
+                        >
+                          {option.label}
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+              {/* size filter */}
+              <AccordionItem value="size">
+                <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
+                  <span className="font-medium text-gray-900">Size</span>
+                </AccordionTrigger>
+                <AccordionContent className="pb-6 animate-none">
+                  <ul className="space-y-4">
+                    {SIZE_FILTER.options.map((option, optionIdx) => (
+                      <li key={option.value} className="flex items-center">
+                        <input
+                          type="checkbox"
+                          id={`size-${optionIdx}`}
+                          onChange={() => {
+                            applyArrayFilter({
+                              category: "size",
+                              value: option.value
+                            })
+                          }}
+                          checked={filter.size.includes(option.value)}
                           className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                         />
                         <label
