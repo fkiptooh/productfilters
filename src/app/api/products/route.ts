6bd83cabd9bd29d2a54fbc68303e0e86705f8afa
Felix Kiptoo <fkiptooh@gmail.com>
2024-04-16 00:35:10 +0300

    feat(api) - added a filter class

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,44 +1,48 @@
-import { db } from "@/db"
+import { db } from "@/db";
 import { ProductFilterValidator } from "@/lib/validator/product-validator";
 import { NextRequest, NextResponse } from "next/server";
 
 class Filter {
-    private filters: Map<string, string[]> = new Map();
+  private filters: Map<string, string[]> = new Map();
 
-    hasFilter() {
-        return this.filters.size > 0;
-    }
+  hasFilter() {
+    return this.filters.size > 0;
+  }
 
-    add(key: string, operator: string, value: string | number) {
-        const filter = this.filters.get(key) || [];
+  add(key: string, operator: string, value: string | number) {
+    const filter = this.filters.get(key) || [];
 
-        filter.push(`${key} ${operator} ${typeof value === "number" ? value : `"${value}"`}`);
+    filter.push(
+      `${key} ${operator} ${typeof value === "number" ? value : `"${value}"`}`
+    );
 
-        this.filters.set(key, filter);
-    }
+    this.filters.set(key, filter);
+  }
 
-    addRaw(key: string, rawFilter: string) {
-        this.filters.set(key, [rawFilter]);
-    }
+  addRaw(key: string, rawFilter: string) {
+    this.filters.set(key, [rawFilter]);
+  }
 
-    get() {
-        const parts: string[] = [];
-        this.filters.forEach((filter) => {
-            const groupedValue = filter.join(` OR `)
-            parts.push(`(${groupedValue})`);
-        })
-        return parts.join(' AND ');
-    }
+  get() {
+    const parts: string[] = [];
+    this.filters.forEach((filter) => {
+      const groupedValue = filter.join(` OR `);
+      parts.push(`(${groupedValue})`);
+    });
+    return parts.join(" AND ");
+  }
 }
 export const POST = async (req: NextRequest) => {
-    const body = await req.json();
-    const { color, price, size, sort } = ProductFilterValidator.parse(body.filter)
-    const products = await db.query({
-        topK: 12,
-        vector: [0, 0, 0],
-        includeMetadata: true
-    });
+  const body = await req.json();
+  const { color, price, size, sort } = ProductFilterValidator.parse(
+    body.filter
+  );
+  const products = await db.query({
+    topK: 12,
+    vector: [0, 0, 0],
+    includeMetadata: true,
+  });
 
-    return NextResponse.json(products)
-    // return new Response(JSON.stringify(products))
-}
+  return NextResponse.json(products);
+  // return new Response(JSON.stringify(products))
+};
