e805270e7cf27274c10216b3e0aff2ff456f2b5d
Felix Kiptoo <fkiptooh@gmail.com>
2024-04-16 00:34:39 +0300

    feat(api) - added a filter class

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,7 +1,38 @@
 import { db } from "@/db"
-import { NextResponse } from "next/server";
+import { ProductFilterValidator } from "@/lib/validator/product-validator";
+import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async () => {
+class Filter {
+    private filters: Map<string, string[]> = new Map();
+
+    hasFilter() {
+        return this.filters.size > 0;
+    }
+
+    add(key: string, operator: string, value: string | number) {
+        const filter = this.filters.get(key) || [];
+
+        filter.push(`${key} ${operator} ${typeof value === "number" ? value : `"${value}"`}`);
+
+        this.filters.set(key, filter);
+    }
+
+    addRaw(key: string, rawFilter: string) {
+        this.filters.set(key, [rawFilter]);
+    }
+
+    get() {
+        const parts: string[] = [];
+        this.filters.forEach((filter) => {
+            const groupedValue = filter.join(` OR `)
+            parts.push(`(${groupedValue})`);
+        })
+        return parts.join(' AND ');
+    }
+}
+export const POST = async (req: NextRequest) => {
+    const body = await req.json();
+    const { color, price, size, sort } = ProductFilterValidator.parse(body.filter)
     const products = await db.query({
         topK: 12,
         vector: [0, 0, 0],
